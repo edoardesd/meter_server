e10416f8d373cc730e91c91a39cc81880d81c79d
edoardesd <edoardesd@gmail.com>
2023-02-17 10:10:06 +0100

    add chart update without refresh

--- chart/js_chart/app.js
+++ chart/js_chart/app.js
@@ -1,12 +1,8 @@
 var meterRegular = [], meterForward = []
 const xlabels = ['From sensor 0', 'From sensor 1']
 
-async function meterChart() {
-  await getMeterData()
 
-const ctx = document.getElementById('myChart').getContext('2d');
-
-const chart = new Chart(ctx, {
+const config = {
     // The type of chart we want to create
     type: 'bar',
 
@@ -17,15 +13,14 @@ const chart = new Chart(ctx, {
             {
               label: 'Regular',
               backgroundColor: '#f87979',
-              data: meterRegular
+            //   data: meterRegular
             },
             {
               label: 'Forward',
               backgroundColor: '#ff0088',
-              data: meterForward
-            },
-            
-          ],
+            //   data: meterForward
+            }
+          ]
     },
 
     options: {
@@ -41,30 +36,63 @@ const chart = new Chart(ctx, {
                 }]
         }
     }
-})
-
-setTimeout(meterChart, 5000);
-
 }
 
-meterChart()
+// async function meterChart() {
+//   await getMeterData()
 
+
+
+
+// setTimeout(meterChart, 5000);
+
+// }
+
+// meterChart()
+
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart = new Chart(ctx, config);
+updateChart()
+
+
+function updateChart(){
+    async function fetchData(){
+        const apiUrl = "http://127.0.0.1:8000/meter_status";
+        const response = await fetch(apiUrl);
+        const barChatData = await response.json();
+        return barChatData;
+    }
+
+    fetchData().then(barChatData => { 
+        const barChartRegular = barChatData.regular;
+        const barChartForward = barChatData.forward;
+
+        chart.config.data.datasets[0].data = barChartRegular;
+        chart.config.data.datasets[1].data = barChartForward;
+        chart.update();
+    })
+
+    console.log('update');
+};
+setInterval(updateChart, 5000);
 
 
 //Fetch Data from API
 
-async function getMeterData() {
-  const apiUrl = "http://127.0.0.1:8000/meter_status"
+// async function getMeterData() {
+//   const apiUrl = "http://127.0.0.1:8000/meter_status"
 
-  const response = await fetch(apiUrl)
-  const barChatData = await response.json()
+//   const response = await fetch(apiUrl)
+//   const barChatData = await response.json()
   
-  console.log(barChatData)
-  const barChartRegular = barChatData.regular
-  const barChartForward = barChatData.forward
+//   console.log(barChatData)
+//   const barChartRegular = barChatData.regular
+//   const barChartForward = barChatData.forward
 
-  meterRegular = barChartRegular
-  meterForward = barChartForward
+//   meterRegular = barChartRegular
+//   meterForward = barChartForward
 
-  console.log(meterRegular, meterForward)
-}
+//   console.log(meterRegular, meterForward)
+// }
